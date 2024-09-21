fd82f73ddd7e20a0a0dacb5bdd84bee7a7f6b2e2
sashathelambo <166897058+sashathelambo@users.noreply.github.com>
2024-09-21 13:58:36 -0500

    Update Payroll.java

--- src/main/java/com/csc/Payroll.java
+++ src/main/java/com/csc/Payroll.java
@@ -49,29 +49,25 @@ public class Payroll {
                 System.out.println("  (4) married with children plan");
                 lifeInsurancePlan = scanner.nextInt();
 
-                switch (lifeInsurancePlan) {
-                    case 1:
-                        lifeInsuranceCost = NO_PLAN_COST;
-                        break;
-                    case 2:
-                        lifeInsuranceCost = SINGLE_PLAN_COST;
-                        break;
-                    case 3:
-                        lifeInsuranceCost = MARRIED_PLAN_COST;
-                        break;
-                    case 4:
+                lifeInsuranceCost = switch (lifeInsurancePlan) {
+                    case 1 -> NO_PLAN_COST;
+                    case 2 -> SINGLE_PLAN_COST;
+                    case 3 -> MARRIED_PLAN_COST;
+                    case 4 -> {
                         if (dependents > 0) {
-                            lifeInsuranceCost = MARRIED_WITH_CHILDREN_PLAN_COST;
+                            yield MARRIED_WITH_CHILDREN_PLAN_COST;
                         } else {
                             System.out.println("Sorry! You need at least one child to select that plan.");
                             lifeInsurancePlan = -1; // Invalid selection
+                            yield 0.00;
                         }
-                        break;
-                    default:
+                    }
+                    default -> {
                         System.out.println("Invalid selection. Please try again.");
                         lifeInsurancePlan = -1; // Invalid selection
-                        break;
-                }
+                        yield 0.00;
+                    }
+                };
             } while (lifeInsurancePlan == -1);
 
             // Calculate gross pay
